2e76d7e34bdf0a21566c680971b8a290438ffc1b
Ben <fishersengima@gmail.com>
2019-12-20 01:45:45 -0500

    Php and Sql Testing

---
+++ instructor/checkInstructor.php
@@ -0,0 +1,42 @@
+<?php
+
+
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+
+
+
+$link = mysqli_connect("localhost", "fhscjvrp_instructor", "hsek12inus", "fhscjvrp_userdata");
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM themeSurvey";
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+      while($row = mysqli_fetch_row($result)) {
+        foreach($row as $column => $value) {
+          if($value == hash('sha256', $user."_".$pass)){
+            echo "Success";
+          }
+    }
+}
+        }else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+
+// Close connection
+mysqli_close($link);
+
+
+
+
+
+
+
+
+
+ ?>
