5aef967fe30708519b227cede4d40afa2eab1b40
Ben <fishersengima@gmail.com>
2019-12-20 01:48:34 -0500

    Whoops

--- instructor/checkInstructor.php
+++ instructor/checkInstructor.php
@@ -11,7 +11,7 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-$sql = "SELECT * FROM themeSurvey";
+$sql = "SELECT * FROM instructor_data";
 if($result = mysqli_query($link, $sql)){
     if(mysqli_num_rows($result) > 0){
       while($row = mysqli_fetch_row($result)) {
