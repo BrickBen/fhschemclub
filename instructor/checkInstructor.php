2b313fb6dea834c04e6ff6b4a6c23c638a0a8d47
Ben <fishersengima@gmail.com>
2020-02-09 18:28:37 -0500

    Registration Update

--- instructor/checkInstructor.php
+++ instructor/checkInstructor.php
@@ -12,18 +12,21 @@ if($link === false){
 }
 
 
-
+$inVal = 0;
 $sql = "SELECT * FROM instructor_data";
 if($result = mysqli_query($link, $sql)){
     if(mysqli_num_rows($result) > 0){
       while($row = mysqli_fetch_row($result)) {
         foreach($row as $column => $value) {
           if($value == hash('sha256', $user."_".$pass)){
-            echo "Success";
-            exit();
+            inVal = 1;
           }
     }
-    header("Location: index.php?Message=" . urlencode($Message));
+    if(inval == 1){
+      header("Location: chemicaldata/"); 
+    }else{
+      header("Location: index.php?Message=" . urlencode($Message));
+    }
 }
         }else{
         echo "No records matching your query were found.";
