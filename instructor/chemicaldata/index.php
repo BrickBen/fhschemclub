b5f155ee11fde14ca21b3bd19acf7abe3dfc7555
Ben <fishersengima@gmail.com>
2020-02-23 18:38:53 -0500

    CSS Updates

--- instructor/chemicaldata/index.php
+++ instructor/chemicaldata/index.php
@@ -46,7 +46,7 @@ if (is_writable($filename)) {
     <title>Material Data</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="../../chemicaldata.css">
+    <link rel="stylesheet" href="../../csss/chemicaldata.css">
     <!-- ../../css/ -->
     <script>
         window.console = window.console || function(t) {};
